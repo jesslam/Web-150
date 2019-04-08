adfc37d280c1950b61604b472e78fc2ef6de8eaf
jesslam <49288633+jesslam@users.noreply.github.com>
2019-04-08 14:45:24 -0700

    Add files via upload

---
+++ web150/homework/assignment1.js
@@ -0,0 +1,15 @@
+            
+//create three variables to store info
+var message;
+var firstNum;
+var secondNum;
+var total;
+            
+//initialize
+message = "Welcome to my calculator";
+firstNum = 5;
+secondNum = 7;
+            
+//calculate total
+total = firstNum + secondNum;
+document.write(message + "<br>" + "The sum of " + firstNum + " + " + secondNum + " is : " + total);
