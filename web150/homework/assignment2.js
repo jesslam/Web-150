1e5d44c6a0eb8ba4d06855fac7a92c8b49052375
jesslam <49288633+jesslam@users.noreply.github.com>
2019-04-10 11:19:57 -0700

    Add files via upload

---
+++ web150/homework/assignment2.js
@@ -0,0 +1,17 @@
+//get user input for name
+var name = name.getElementbyID;
+(window.prompt("Let's play the sum game." + "\nPlease enter your name"));
+console.log(name);
+
+//get user input for two numbers
+var num1 = document.getElementById(num1);
+(window.prompt("Enter first number"));
+console.log(num1);
+
+var num2 = document.getElementById(num2);
+(window.prompt("Enter second number"));
+console.log(num2);
+var total = num1 + num2
+
+//initialize
+document.write("Hi, " + name + "! " + "The sum of " + num1 + " + " + num2 + " is " + total);
